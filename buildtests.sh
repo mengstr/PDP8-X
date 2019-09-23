ceb42268341c617e3dd9c54b46faa0b5db52b5a0
Mats Engstrom <mats.engstrom@gmail.com>
2019-09-23 20:27:54 +0200

    Runs D0IB JMPJMS

--- buildtests.sh
+++ buildtests.sh
@@ -6,10 +6,13 @@ dd if=/dev/zero of=img/blank4KW.eep bs=2 count=4096
 tools/tape2bin.sh < bin/D0AB-InstTest-1.pt >img/D0AB-InstTest-1.eep
 
 tools/tape2bin.sh < bin/D0BB-InstTest-2.pt >img/D0BB-InstTest-2.eep
- 
-cat 	img/D0AB-InstTest-1.eep \
+
+tools/tape2bin.sh < bin/D0IB-JMPJMS.pt >img/D0IB-JMPJMS.eep
+
+cat \
+	img/D0AB-InstTest-1.eep \
 	img/D0BB-InstTest-2.eep \
-	img/blank4KW.eep \
+	img/D0IB-JMPJMS.eep \
 	img/blank4KW.eep \
 	img/blank4KW.eep \
 	img/blank4KW.eep \
