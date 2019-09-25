edbd69be399628839362c969a9b1aabed47360a8
Mats Engstrom <mats.engstrom@gmail.com>
2019-09-25 12:09:03 +0200

    ROM for 32 images

--- buildtests.sh
+++ buildtests.sh
@@ -1,64 +1,62 @@
 #!/bin/bash
 
+declare -a arr=(
+	"printhelp.bin          PC:0200 - This help"
+	"D0AB-InstTest-1.pt     PC:0200, cont at 0147 - "
+	"D0BB-InstTest-2.pt     PC:0200 - "
+	"D0IB-JMPJMS.pt         PC:0200 - "
+	"D0JB-JMPJMS-RANDOM.pt  PC:0200 - "
+	"D0DB-RandomAND.pt      PC:0200 - "
+	"D0CC-AddTest.pt        PC:0200 - "
+	"FOCAL-8-NOINT.bn       PC:0200 - "
+)
 
-dd if=/dev/zero of=img/blank4KW.eep bs=2 count=4096
+ord() {
+  LC_CTYPE=C printf '%d' "'$1"
+}
 
-#0
-tools/tape2bin.sh < bin/D0AB-InstTest-1.pt >img/D0AB-InstTest-1.eep
+cp asm/printhelp.base asm/printhelp.pal
+# Convert array text into PAL assembly
+for (( cnt=0; cnt<${#arr[@]}; cnt++ )); do
+	full=${arr[cnt]}
+	msg="$(printf "%02o: %s\n" $cnt "$full")"
+	for (( i=0; i<${#msg}; i++ )); do
+		v=$(ord "${msg:$i:1}")
+		vo=$(printf "%04o" $v)
+		echo "    $vo   / ${msg:$i:1}" >> asm/printhelp.pal
+	done
+	echo "    0015   / Newline" >> asm/printhelp.pal
+	echo "" >> asm/printhelp.pal
+done
+echo '    "<;"e;"n;"d;">;0015' >> asm/printhelp.pal
+echo "    0" >> asm/printhelp.pal
+echo "$" >> asm/printhelp.pal
+tools/palbart asm/printhelp.pal
+mv -f asm/printhelp.bin bin/
 
-#1
-tools/tape2bin.sh < bin/D0BB-InstTest-2.pt >img/D0BB-InstTest-2.eep
+# Interate over the array of files and process them
+# where the source is newer than the destination file
+for src in ${arr[@]}; do
+	dst="${src%.*}".eep
+	if [[ "bin/$src" -nt "img/$dst" ]]; then
+		tools/tape2bin.sh < bin/$src >img/$dst
+	fi
+done
 
-#2
-tools/tape2bin.sh < bin/D0IB-JMPJMS.pt >img/D0IB-JMPJMS.eep
+# Create an empty "filler" file
+dd if=/dev/zero of=img/blank4KW.eep bs=2 count=4096 2&> /dev/null
 
-#3
-tools/tape2bin.sh < bin/D0JB-JMPJMS-RANDOM.pt >img/D0JB-JMPJMS-RANDOM.eep
+# Copy all resultings files into the multitest -file
+rm -f img/32xMultitest.eep
+touch img/32xMultitest.eep
+for src in "${arr[@]}"; do
+	dst="${src%.*}".eep
+	cat img/$dst >> img/32xMultitest.eep
+done
 
-#4
-tools/tape2bin.sh < bin/D0DB-RandomAND.pt >img/D0DB-RandomAND.eep
-
-#5
-tools/tape2bin.sh < bin/D0CC-AddTest.pt > img/D0CC-AddTest.eep
-
-#6
-#tools/tape2bin.sh < bin/_____.pt > img_____.eep
-
-#31 
-tools/tape2bin.sh < bin/FOCAL-8-NOINT.bn > img/FOCAL8NOI.eep
-
-cat \
-	img/D0AB-InstTest-1.eep \
-	img/D0BB-InstTest-2.eep \
-	img/D0IB-JMPJMS.eep \
-	img/D0JB-JMPJMS-RANDOM.eep \
-	img/D0DB-RandomAND.eep \
-	img/D0CC-AddTest.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/blank4KW.eep \
-	img/FOCAL8NOI.eep \
-	> img/multitest128KW.eep
-	
+# Fill up the remainder of the 32 slots with blank images 
+cnt=${#arr[@]}
+while [ $cnt -lt 32 ]; do
+	cat img/blank4KW.eep >> img/32xMultitest.eep
+	((cnt++))
+done
--- testall.sh
+++ testall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-tools/palbart asm/testall.pa || exit 1
-tools/tape2bin.sh < asm/testall.bin >img/testall.bin
+tools/palbart -d -x asm/testall.pa || exit 1
+tools/tape2bin.sh < asm/testall.bin >img/testall.eep
+tools/tapedump.sh < asm/testall.bin >asm/testall.dmp
